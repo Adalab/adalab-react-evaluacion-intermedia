d13a0b1ed1a1919ba49e907beba1d880d2fa05cf
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-04-26 09:05:13 +0200

    paint htmlQuotes in HTML

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
 import '../styles/App.scss';
 import friendsInfo from '../data/friends_info.json';
 import { useState } from 'react';
-console.log(quotes);
-
-
 
 function App() {
+  const [quotes, setQuotes] = useState([friendsInfo]);
 
-
+  const htmlQuotes = friendsInfo.map((quote, i) => {
+    return (
+      <li key={i}>
+        <strong> {quote.quote} </strong> - <em> {quote.character} </em>
+      </li>
+    );
+  });
 
   return (
     <div className='App'>
       <h1>Frases de Friends</h1>
       <div>
         <ul>
-
+          {htmlQuotes}
         </ul>
       </div>
     </div>
