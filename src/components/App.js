dc583d539097f2e313f0f1ca41315c47333f5592
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-04-26 10:09:02 +0200

    filter quote by change on search input working

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,12 @@ import friendsInfo from '../data/friends_info.json';
 import { useState } from 'react';
 
 function App() {
-  console.log(friendsInfo);
   const [quotes, setQuotes] = useState(friendsInfo);
   const [newQuote, setnewQuote] = useState({
     quote: '',
     character: '',
   });
+  const [search, setSearch] = useState('');
 
   const handleAddBtn = (ev) => {
     ev.preventDefault();
@@ -27,8 +27,15 @@ function App() {
     });
   };
 
+  const handleSearch = (ev) => {
+    setSearch(ev.target.value)
+    console.log(search);
+  }
 
-  const htmlQuotes = quotes.map((quote, i) => {
+
+  const htmlQuotes = quotes
+  .filter((quote) => quote.quote.toLocaleLowerCase().includes(search))
+  .map((quote, i) => {
     return (
       <li key={i}>
         <strong> {quote.quote} </strong> - <em> {quote.character} </em>
@@ -39,6 +46,23 @@ function App() {
   return (
     <div className='App'>
       <h1>Frases de Friends</h1>
+
+      <div>
+      <label htmlFor="search">Filtrar por frase</label>
+      <input onChange={handleSearch} type="text" name="search" id="search" value={search} placeholder='¡Ay caramba!'/>
+      
+      <label htmlFor="">Filtrar por personaje</label>
+      <select name="select">
+        <option value="value0" defaultValue>Todos</option>
+        <option value="value1">Ross</option>
+        <option value="value2">Monica</option>
+        <option value="value2">Joey</option>
+        <option value="value2">Phoebe</option>
+        <option value="value2">Rachel</option>
+      </select>
+
+    </div>
+
       <div>
         <ol>{htmlQuotes}</ol>
       </div>
