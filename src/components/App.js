67c0c766b2ff07d31cec6f1655ffbcee799707fd
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-04-26 08:58:31 +0200

    initial commit + imports at app.js

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import '../styles/App.scss';
+import friendsInfo from '../data/friends_info.json';
+import { useState } from 'react';
+console.log(quotes);
+
+
+
+function App() {
+
+
+
+  return (
+    <div className='App'>
+      <h1>Frases de Friends</h1>
+      <div>
+        <ul>
+
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
